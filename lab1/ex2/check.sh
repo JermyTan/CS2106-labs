7edad464ab7308efb65e0e45d52bf64017eea384
Jeremy Tan <tan_kai_qun97@yahoo.com.sg>
2021-08-22 17:57:25 +0800

    Update check script to include mem leak checks

--- lab1/ex2/check.sh
+++ lab1/ex2/check.sh
@@ -5,3 +5,10 @@ make
 ./ex2 < sample.in | diff sample.out -
 ./ex2 < small_test.in | diff small_test.out -
 ./ex2 < big_test.in | diff big_test.out -
+
+if command -v valgrind
+then
+    valgrind ./ex2 < sample.in
+    valgrind ./ex2 < small_test.in
+    valgrind ./ex2 < big_test.in
+fi
--- lab1/ex3/check.sh
+++ lab1/ex3/check.sh
@@ -5,3 +5,10 @@ make
 ./ex3 sample.in | diff sample.out -
 ./ex3 small_test.in | diff small_test.out -
 ./ex3 big_test.in | diff big_test.out -
+
+if command -v valgrind
+then
+    valgrind ./ex3 sample.in
+    valgrind ./ex3 small_test.in
+    valgrind ./ex3 big_test.in
+fi
