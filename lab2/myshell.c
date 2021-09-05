0845c6b6924203228d6525454507c35f60cd7581
Jeremy Tan <tan_kai_qun97@yahoo.com.sg>
2021-09-05 20:50:21 +0800

    Add io redirection functionalities

--- lab2/myshell.c
+++ lab2/myshell.c
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "myshell.h"
@@ -22,6 +24,10 @@
 #define TERMINATING 2
 #define BACKGROUND_TASK_FLAG "&"
 #define AND_OPERATOR "&&"
+#define INPUT_REDIR_OPERATOR "<"
+#define OUTPUT_REDIR_OPERATOR ">"
+#define ERROR_REDIR_OPERATOR "2>"
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 typedef struct
 {
@@ -59,16 +65,45 @@ int get_shell_command_id(char *command)
 }
 
 // returns 1 if should run program in background else 0
-int check_should_run_in_background(char **args, size_t num_args)
+int check_should_run_in_background(char **args, size_t *num_args)
 {
-    if (num_args <= 1 || strcmp(args[num_args - 1], BACKGROUND_TASK_FLAG) != 0)
+    if (*num_args <= 1 || strcmp(args[*num_args - 1], BACKGROUND_TASK_FLAG) != 0)
     {
         return 0;
     }
 
     // remove "&" from args
-    args[num_args - 1] = NULL;
+    args[--(*num_args)] = NULL;
     return 1;
+}
+
+void check_redirection_files(char **args, size_t *num_args, char **input_file, char **output_file, char **error_file)
+{
+    size_t original_num_args = *num_args, i = 0;
+
+    while (i < original_num_args)
+    {
+        if (strcmp(args[i], INPUT_REDIR_OPERATOR) == 0 && *input_file == NULL)
+        {
+            args[i] = NULL;
+            *num_args = MIN(i, *num_args);
+            *input_file = args[++i];
+        }
+        else if (strcmp(args[i], OUTPUT_REDIR_OPERATOR) == 0 && *output_file == NULL)
+        {
+            args[i] = NULL;
+            *num_args = MIN(i, *num_args);
+            *output_file = args[++i];
+        }
+        else if (strcmp(args[i], ERROR_REDIR_OPERATOR) == 0 && *error_file == NULL)
+        {
+            args[i] = NULL;
+            *num_args = MIN(i, *num_args);
+            *error_file = args[++i];
+        }
+
+        i++;
+    }
 }
 
 process *get_child_process(pid_t pid)
@@ -155,12 +190,32 @@ void exec_terminate(pid_t pid)
 
 // returns the exit status of the executed program
 // 0 is returned if the executed program runs in background
-int exec_program(char *program, char **args, int should_run_in_background)
+int exec_program(char *program, char **args, int should_run_in_background, char *input_file, char *output_file, char *error_file)
 {
     pid_t pid = fork();
 
     if (pid == 0)
     {
+        int in_fd = input_file ? open(input_file, O_RDONLY) : STDIN_FILENO;
+        int out_fd = output_file ? open(output_file, O_WRONLY | O_CREAT, 0777) : STDOUT_FILENO;
+        int err_fd = error_file ? open(error_file, O_WRONLY | O_CREAT, 0777) : STDERR_FILENO;
+
+        if (in_fd != STDIN_FILENO)
+        {
+            dup2(in_fd, STDIN_FILENO);
+            close(in_fd);
+        }
+        if (out_fd != STDOUT_FILENO)
+        {
+            dup2(out_fd, STDOUT_FILENO);
+            close(out_fd);
+        }
+        if (err_fd != STDERR_FILENO)
+        {
+            dup2(err_fd, STDERR_FILENO);
+            close(err_fd);
+        }
+
         execv(program, args);
         exit(1);
     }
@@ -221,8 +276,24 @@ int exec_command(char **args, size_t num_args)
             return 1;
         }
 
-        int exit_status = exec_program(command, args, check_should_run_in_background(args, num_args));
-        if (exit_status != 0)
+        int should_run_in_background = check_should_run_in_background(args, &(num_args));
+
+        char *input_file = NULL, *output_file = NULL, *error_file = NULL;
+        check_redirection_files(args, &(num_args), &(input_file), &(output_file), &(error_file));
+
+        if (input_file && access(input_file, F_OK) != 0)
+        {
+            printf("%s does not exist\n", input_file);
+            return 1;
+        }
+
+        if (exec_program(
+                command,
+                args,
+                should_run_in_background,
+                input_file,
+                output_file,
+                error_file) != 0)
         {
             printf("%s failed\n", command);
             return 1;
